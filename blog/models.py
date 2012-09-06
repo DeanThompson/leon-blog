fae49d11fd15b050ffd13d472164fe5af2703e3e
Yangliang Li <yanglianglee@gmail.com>
2012-09-06 22:15:29 +0800

    1. create Blog and Category models 2. install app taggit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+from taggit.managers import TaggableManager
+
+from datetime import datetime
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = ('category')
+        verbose_name_plural = ('categories')
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return 'leon_category', None, {'category': self.slug}
+    get_absolute_url = models.permalink(get_absolute_url)
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    slug = models.SlugField()   # more readable url
+    category = models.ForeignKey(Category, related_name='blogs', blank=True, \
+                    null=True, default=None, on_delete=models.SET_NULL)
+    tags = TaggableManager(blank=True)
+    pub_date = models.DateTimeField(default=datetime.now())
+
+    class Meta:
+        ordering = ('-pub_date',)
+        get_latest_by = 'pub_date'
+        verbose_name = ('blog')
+        verbose_name_plural = ('blogs')
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return 'leon_blog', None, {'year': self.pub_date.year,
+                                   'month': '%02d' % self.pub_date.month,
+                                   'slug': self.slug}
+    get_absolute_url = models.permalink(get_absolute_url)
--- leon_blog/settings.py
+++ leon_blog/settings.py
@@ -127,9 +127,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+
+    'taggit',
+
     'blog',
 )
 
