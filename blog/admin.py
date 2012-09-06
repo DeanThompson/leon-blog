6703a2903de36a5cd96202aaf1dc8dacbfdcf6d7
Yangliang Li <yanglianglee@gmail.com>
2012-09-06 22:38:21 +0800

    register Blog and Category to admin.site

---
+++ blog/admin.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+__author__ = 'leon'
+
+from django.db import models
+from django.contrib import admin
+
+from models import Blog, Category
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'slug', 'pub_date', 'category',)
+    search_fields = ('title', 'content',)
+    list_filter = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug',)
+    search_fields = ('title', 'slug',)
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
