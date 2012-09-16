886d01aacefcbafc62912dddff85b80f6504c2c9
Yangliang Li <yanglianglee@gmail.com>
2012-09-16 17:19:20 +0800

    add recently posted blogs in sidebar.

--- blog/templatetags/customer_tags.py
+++ blog/templatetags/customer_tags.py
@@ -9,6 +9,15 @@ from blog.models import Category
 
 register = Library()
 
+@register.inclusion_tag("blog/sidebar_items/latest.html")
+def latest_posts():
+    latest_posts = Blog.objects.select_related(depth=1).order_by("-pub_date")
+    return {
+        'template': "blog/sidebar/items/latest.html",
+        'latest_posts': latest_posts[:5],
+    }
+
+
 @register.inclusion_tag("blog/sidebar_items/categories.html")
 def categories():
     ''' 自定义的 包含标签，用于显示所有的分类 '''
@@ -16,6 +25,7 @@ def categories():
         'template': "blog/sidebar_items/categories.html",
         'categories': Category.objects.order_by('title'),
     }
+
 
 @register.inclusion_tag("blog/sidebar_items/archives.html")
 def archives():
